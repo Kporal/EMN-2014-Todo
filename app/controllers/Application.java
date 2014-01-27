bd2c4566e56dca1dd24f8452c50c7daf40b63f56
Kporal <kporal@gmx.fr>
2014-01-27 15:26:39 +0100

    Add + remove tasks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,4 +10,30 @@ import models.*;
 
 public class Application extends Controller {
 
+	/**
+	 * Default action. render the app/views/Application/index.html template
+	 */
+	public static void index() {
+		List<Task> tasks = Task.findAll();
+		render(tasks);
+	}
+
+	/**
+	 * Add a new task.
+	 */
+	public static void add(String name) {
+		Task task = new Task(name);
+		task.save();
+		renderJSON(task);
+	}
+	
+	/**
+	 * Remove a task.
+	 */
+	public static void delete(Long id) {
+		Task task = Task.findById(id);
+		task.done = true;
+		task.save();
+		renderJSON(task);
+	}
 }
